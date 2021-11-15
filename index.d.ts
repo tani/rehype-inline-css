6e7802e5b12bf25342b528e48ec4090836d22091
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-11-15 13:50:02 +0000

    Delete files

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Options } from 'juice';
-import type { Root } from 'hast';
-declare const _default: (options: Options) => (tree: Root) => import("unist").Node<import("unist").Data>;
-export default _default;
