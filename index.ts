5fbbea3495019d46d6c1951715561d618f934412
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-11-15 13:52:02 +0000

    Options is option

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { parseFragment } from 'parse5'
 import juice, { Options} from 'juice'
 import type { Root } from 'hast'
 
-export default (options: Options) => function transformer(tree: Root) {
+export default (options?: Options) => function transformer(tree: Root) {
     const html = toHtml(tree)
     const src = juice(html, options)
     const doc = parseFragment(src)
