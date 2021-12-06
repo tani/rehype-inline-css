4ecdf530cc3446b0cb22cbd6e748b2af65a6d156
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-12-06 13:40:47 +0000

    Drop juice

--- index.ts
+++ index.ts
@@ -1,13 +1,31 @@
-import fromParse5 from 'hast-util-from-parse5'
-import toHtml from 'hast-util-to-html'
-import { parseFragment } from 'parse5'
-import juice, { Options} from 'juice'
 import type { Root } from 'hast'
+import { selectAll } from "hast-util-select"
+import { toString } from "hast-util-to-string";
+import css from "css"
+import { remove } from "unist-util-remove"
 
-export default (options?: Options) => function transformer(tree: Root) {
-    const html = toHtml(tree)
-    const src = juice(html, options)
-    const doc = parseFragment(src)
-    const hast = fromParse5(doc)
-    return hast
+export default () => function transformer(tree: Root) {
+    const stylesheet = selectAll('style', tree).map(toString).join("\n")
+    const cast = css.parse(stylesheet)
+    for (const rule of cast.stylesheet?.rules ?? []) {
+        if ("selectors" in rule) {
+            for(const selector of rule.selectors ?? []) {
+                const elems = selectAll(selector, tree)
+                for(const elem of elems) {
+                    for(const declaration of rule.declarations ?? []) {
+                        if("property" in declaration) {
+                            elem.properties ??= {}
+                            elem.properties.style ??= ""
+                            if(/[^;]\s*$/.test(elem.properties.style as string)) {
+                                elem.properties.style += ";"
+                            }
+                            elem.properties.style += `${declaration.property}:${declaration.value};`
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    return remove(tree, { tagName: "style" })
 }
--- test.js
+++ test.js
@@ -2,11 +2,11 @@ import test from 'tape'
 import { unified } from 'unified'
 import rehype from 'rehype-parse'
 import stringify from 'rehype-stringify'
-import inlineCss from './index.mjs'
-const processor = unified().use(rehype).use(inlineCss).use(stringify)
+import inlineCss from './index.js'
+const processor = unified().use(rehype, { fragment: true }).use(inlineCss).use(stringify)
 test("Inlining a stylesheet", (t) => {
     const i = `<p>test</p><style>p { color: red; }</style>`
-    const e = `<p style="color: red;">test</p>`
+    const e = `<p style="color:red;">test</p>`
     const o = processor.processSync(i).toString()
     t.equal(o, e)
     t.end()
